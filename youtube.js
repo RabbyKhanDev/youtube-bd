7d962687c97ecb678b86aa5b4f68714c8e88d669
Ronok Khan <103366145+Ronokkhan@users.noreply.github.com>
2022-05-09 17:39:20 +0600

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,4 +1,4 @@
-alert('Fake Youtube but works like sh*t!')
+alert('ভুয়া Youtube কিন্তু কাজে আসল!')
 document.getElementById('chg-p2').innerHTML= 'How To Be Honest | Awkward Puppet';
 // var mail = prompt('Enter your email');
 console.log(mail);
