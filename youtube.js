426e07577a635da416fd72abf2c1a212156c450c
Ronokkhan <103366145+Ronokkhan@users.noreply.github.com>
2022-05-09 16:44:10 +0600

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,5 @@
+alert('Fake Youtube but works like sh*t!')
+document.getElementById('chg-p2').innerHTML= 'How To Be Honest | Awkward Puppet';
+// var mail = prompt('Enter your email');
+console.log(mail);
+document.write(mail);
